408c8963ec417d1abf1b17116bbdc3c7ac76ee14
Andres <vittorio15@hotmail.com>
2013-10-19 09:31:08 -0500

    Primera version

---
+++ Turismobile/src/com/turismobile/cargadatos/Hoteles.java
@@ -0,0 +1,15 @@
+package com.turismobile.cargadatos;
+
+import com.turismobile.cargadatos.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Hoteles extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_hotel);
+	}
+	}
